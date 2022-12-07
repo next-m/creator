eae8cc2ae9c6ffb0e31d91e1fc47ac31644c2185
hyoungjinNam <pronhj@gmail.com>
2022-12-07 23:30:32 +0900

    첫번째

--- src/api/common/common.js
+++
@@ -1,8 +0,0 @@
-import { createInstanceWithAuth } from '../index';
-function fileDelete(id) {
-  return createInstanceWithAuth(`/api/h1/file/${id}`, {}, {}, 'application/json; charset=utf-8').delete();
-}
-function creatoryVideCategory(id) {
-  return createInstanceWithAuth(`/api/h1/syscode/parents/${id}`, {}, {}, 'application/json; charset=utf-8').get();
-}
-export { fileDelete, creatoryVideCategory };
